5fa38cb0b131843b4d44a49c75463499061569f6
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-02-28 18:26:45 +0600

    created all methods for an event

--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -19,6 +19,41 @@ eventRoutes.post("/", verifyUser, async (req, res) => {
   res.send(event);
 });
 
+eventRoutes.get("/", verifyUser, async (req, res) => {
+  const events = await Event.find({});
+  res.send(events);
+});
+
+eventRoutes.get("/:id", verifyUser, async (req, res) => {
+  const { id } = req.params;
+  const event = await Event.findById(id);
+  res.send(event);
+});
+
+eventRoutes.put("/:id", verifyUser, async (req, res) => {
+  const { id } = req.params;
+  const { summary, description, start, end, status, priority } = req.body;
+  const event = await Event.findByIdAndUpdate(
+    id,
+    {
+      summary,
+      description,
+      start,
+      end,
+      status,
+      priority,
+    },
+    { new: true }
+  );
+  res.send(event);
+});
+
+eventRoutes.delete("/:id", verifyUser, async (req, res) => {
+  const { id } = req.params;
+  const event = await Event.findByIdAndDelete(id);
+  res.send(event);
+});
+
 export default eventRoutes;
 
 // TODO
