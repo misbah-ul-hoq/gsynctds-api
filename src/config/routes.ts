0473d025766a8103a74071761ac92b342ae46548
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-02-28 18:00:46 +0600

    added events router in routes

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,8 +1,10 @@
 import { Express } from "express";
 import { authRoutes } from "../routes/authRoutes";
+import eventRoutes from "../routes/eventRoutes";
 
 const routes = (app: Express) => {
   app.use("/auth", authRoutes);
+  app.use("/events", eventRoutes);
 };
 
 export default routes;
